12d2c616050a681544377091bae52ebee9348fb9
muqeetqazi <muqeetahmad202@gmail.com>
2025-09-13 17:52:38 +0500

    Configure DATABASES to use dj-database-url for Postgres in production

--- core/settings.py
+++ core/settings.py
@@ -69,10 +69,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
+        ssl_require=False  # DigitalOcean will set sslmode via DATABASE_URL
     )
 }
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
